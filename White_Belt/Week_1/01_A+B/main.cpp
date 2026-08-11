41c1c19cfd1c9e25d53d2d3f79e8d4e06a16df59
xSeventy <vsevolodvp10@gmail.com>
2026-08-11 14:35:36 +0300

    first task

---
+++ White_Belt/Week_1/01_A+B/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+	int x = 0;
+	int y = 0;
+    
+	cin >> x >> y;
+	
+    int sum = 0;
+	
+    sum = x + y;
+	cout << sum;
+	
+    return 0;
+}
