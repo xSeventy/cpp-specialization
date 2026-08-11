aa193409e62c1b277c90142fdc97d535b57e297e
xSeventy <vsevolodvp10@gmail.com>
2026-08-11 15:33:39 +0300

    tasks

---
+++ White_Belt/Week_1/02_minimal_string/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string a, b, c;
+    cin >> a >> b >> c;
+
+    if(a <= b && a <= c) {
+        
+        cout << a << endl;
+
+    } else if (b < a && b < c) {
+        
+        cout << b << endl;
+
+    } else {
+        
+        cout << c << endl;
+
+    }
+    
+    return 0;
+}
